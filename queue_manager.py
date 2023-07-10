48cbae29b5a1c750ea1f537b133870da5baaf278
Preston Buterbaugh <pbuterba.corp@gmail.com>
2023-07-09 20:34:57 -0400

    Update process for finding insertion points for multiple adds and customization

--- queue_manager.py
+++ queue_manager.py
@@ -43,6 +43,53 @@ def list_artists(song: Dict) -> str:
     return string[:-2]
 
 
+def str_to_ms(duration: str) -> int:
+    """
+    @brief      Converts a duration string into milliseconds
+    @param      (str) duration: The duration represented as a string
+    @return:    (int): The number of milliseconds represented by the duration, or -1 if an invalid duration string is entered
+    """
+    try:
+        duration_hours = int(duration.split(':')[0])
+        duration_minutes = int(duration.split(':')[1])
+    except ValueError:
+        return -1
+
+    total_minutes = duration_minutes + (duration_hours * 60)
+    return total_minutes * 60000
+
+
+def ms_to_str(ms: int) -> str:
+    """
+    @brief      Converts a number of milliseconds into a duration string hh:mm:ss.ms
+    @param      (int) ms: The total number of milliseconds to represent as a string
+    @return:    (str): The duration represented as a string hh:mm:ss.ms
+    """
+    # Calculate individual values
+    seconds = ms // 1000
+    ms = ms - (seconds * 1000)
+
+    minutes = seconds // 60
+    seconds = seconds - (minutes * 60)
+
+    hours = minutes // 60
+    minutes = minutes - (hours * 60)
+
+    # Add leading zeros
+    if minutes < 10:
+        minutes = f'0{minutes}'
+
+    if seconds < 10:
+        seconds = f'0{seconds}'
+
+    if ms < 10:
+        ms = f'00{ms}'
+    elif ms < 100:
+        ms = f'0{ms}'
+
+    return f'{hours}:{minutes}:{seconds}.{ms}'
+
+
 def get_device(spotify: spotipy.client) -> Dict | None:
     """
     @brief      Function that takes the user through a process of selecting which device to manage
@@ -173,6 +220,11 @@ def main() -> int:
     # Get user device selection
     device = get_device(spotify)
 
+    # Set up variables to track what songs will be added to the queue
+    insert_after_list = {}
+    starting_song = None
+    starting_ms = 0
+
     # Continue adding songs until user enters empty song title
     title = input('Enter the title of a song to add to the queue: ')
     while title != '':
@@ -182,71 +234,76 @@ def main() -> int:
         # Search for the song
         song = find_song(spotify, title, artist)
 
+        if song is not None:
+            # Get duration information
+            total_ms = str_to_ms(input(f'Enter the duration in the queue at which to insert "{song["name"]}", in hh:mm format: '))
+
+            # Check for improperly formatted duration
+            while total_ms == -1:
+                print('Improperly formatted duration. Please try again')
+                total_ms = str_to_ms(input())
+
+            # Check for duration of zero if a starting song has already been set
+            while total_ms == 0 and starting_song is not None:
+                print(f'"{starting_song["name"]} is already set as the starting song. Please enter a new duration:')
+                total_ms = str_to_ms(input())
+
+                # Check for improperly formatted duration
+                while total_ms == -1:
+                    print('Improperly formatted duration. Please try again')
+                    total_ms = str_to_ms(input())
+
+            # Check if song is inserted at the beginning or in the middle of the queue
+            if total_ms > 0:
+                # Set tracking variables
+                ms_elapsed = starting_ms
+                songs_skipped = 0
+
+                # Loop until end point
+                print('Searching for insertion point...')
+                while ms_elapsed < total_ms:
+                    # Get queue
+                    queue = spotify.queue()
+                    while queue['currently_playing'] is None:
+                        print('Could not fetch current song. Try briefly playing and pausing music on your phone so that it is recognized as an active queue')
+                        print('Press RETURN to try again. Enter the value "E" to exit the program.')
+                        check = confirmation()
+                        if not check:
+                            return 0
+
+                    # Get current song
+                    current_song = queue['currently_playing']
+                    print(current_song["name"])
+                    ms_elapsed = ms_elapsed + current_song['duration_ms']
+
+                    # Check if song is meant to have another song inserted after it
+                    if current_song['name'] in insert_after_list.keys():
+                        ms_elapsed = ms_elapsed + insert_after_list[current_song['name']]
+
+                    # Skip to next song
+                    spotify.next_track(device_id=device['id'])
+                    spotify.pause_playback(device_id=device['id'])
+                    songs_skipped = songs_skipped + 1
+
+                # Get insert location information
+                duration_string = ms_to_str(ms_elapsed)
+
+                # Output insertion location
+                print(f'Insert "{song["name"]}" after "{current_song["name"]}" ({duration_string})')
+                insert_after_list[current_song['name']] = song
+
+                for i in range(songs_skipped):
+                    spotify.previous_track(device_id=device['id'])
+                spotify.pause_playback(device_id=device['id'])
+            else:
+                # Song is inserted at the beginning of the queue
+                print(f'"{song["name"]}" will be inserted at the beginning of the queue')
+                starting_song = song
+                starting_ms = song['duration_ms']
+
         # Get next song title
         print('Enter the title of another song, or press RETURN to exit')
-
-    # Get duration information
-    duration = input('Enter the duration to insert "Almost Home" at in hh:mm format: ')
-    duration_hours = int(duration.split(':')[0])
-    duration_minutes = int(duration.split(':')[1])
-    total_minutes = duration_minutes + (duration_hours * 60)
-    total_ms = total_minutes * 60 * 1000
-    songs_skipped = 0
-
-    # URIs for fixed tracks
-    home_uri = 'spotify:track:2aMb1asq5acm7cDYlFsYhY'
-    almost_home_uri = 'spotify:track:4c4Y0MvScCAI98Eum1dwOz'
-
-    # Add Home to queue
-    home = spotify.track(home_uri)
-    ms_elapsed = home['duration_ms']
-
-    if ms_elapsed > total_ms:
-        print('Not enough time in the drive')
-        return 1
-    elif ms_elapsed == total_ms:
-        print('Insert "Almost Home" after "Home"')
-
-    # Loop until end point
-    print('Searching for insertion point...')
-    while ms_elapsed < total_ms:
-        queue = spotify.queue()
-        while queue['currently_playing'] is None:
-            print('Could not fetch current song. Try briefly playing and pausing music on your phone so that it is recognized as an active queue')
-            print('Press ENTER to try again. Press "E" followed by ENTER to cancel.')
-            keyboard = input()
-            if keyboard.lower() == 'e':
-                print('Aborting program')
-                return 0
-            elif keyboard.lower() == '':
-                queue = spotify.queue()
-            else:
-                print('Unrecognized command. Press ENTER to fetch queue again, press "E" followed by ENTER to end program')
-
-        current_song = queue['currently_playing']
-        print(current_song["name"])
-        ms_elapsed = ms_elapsed + current_song['duration_ms']
-        spotify.next_track(device_id=device['id'])
-        spotify.pause_playback(device_id=device['id'])
-        songs_skipped = songs_skipped + 1
-
-    print(f'Insert "Almost Home" after "{current_song["name"]}"')
-
-    # Get insert location information
-    seconds_elapsed = ms_elapsed // 1000
-    ms_elapsed = ms_elapsed - (seconds_elapsed * 1000)
-
-    minutes_elapsed = seconds_elapsed // 60
-    seconds_elapsed = seconds_elapsed - (minutes_elapsed * 60)
-
-    hours_elapsed = minutes_elapsed // 60
-    minutes_elapsed = minutes_elapsed - (hours_elapsed * 60)
-
-    print(f'Insert location is {hours_elapsed}:{minutes_elapsed}:{seconds_elapsed}.{ms_elapsed}')
-
-    for i in range(songs_skipped):
-        spotify.previous_track(device_id=device['id'])
-    spotify.pause_playback(device_id=device['id'])
+        title = input()
 
     # Add Home to queue
     spotify.add_to_queue(home_uri, device_id=device['id'])
